464f5ffa286b370425b26973fa86bf2dc6a7b957
Calvinbrine-Nestoris <calvin.williams@mymail.champlain.edu>
2025-03-02 16:44:11 -0500

    FireballGridGame

--- Assets/Scripts/GridManager.cs
+++ Assets/Scripts/GridManager.cs
@@ -5,10 +5,8 @@ using UnityEngine;
 public class GridManager : MonoBehaviour
 {
     [SerializeField] private GameObject _tilePrefab;
-    [SerializeField] private int numRows;
-    [SerializeField] private int numColumns;
-    [SerializeField] private int minRows;
-    [SerializeField] private int minColumns;
+    [SerializeField] public int numRows;
+    [SerializeField] public int numColumns;
     [SerializeField] private Vector2 tilesize = Vector2.one;
     [SerializeField] private Vector2 padding = new Vector2(0.1f,0.1f);
     private List<GameObject> _tiles = new List<GameObject>();
@@ -16,9 +14,9 @@ public class GridManager : MonoBehaviour
     void Start()
     {
         _tiles.Capacity = numRows * numColumns;
-        for (int row = minRows; row < numRows; row++)
+        for (int row = 0; row < numRows; row++)
         {
-            for (int col = minColumns; col < numColumns; col++)
+            for (int col = 0; col < numColumns; col++)
             {
                 Vector2 pos = new Vector2(col * (tilesize.x + padding.x), row * (tilesize.y + padding.y));
                 GameObject tile = Instantiate(_tilePrefab, pos, Quaternion.identity,transform);
@@ -29,12 +27,6 @@ public class GridManager : MonoBehaviour
 
     public GameObject GetTile(int column, int row)
     {
-        if (column < 0 || row < 0 || column >= numColumns || row >= numRows)
-        {
-            Debug.LogError($"Invalid tile coordinate({column},{row})");
-            return null;
-        }
-        else
         {
             return _tiles[row * numColumns + column];
         }
--- Assets/Scripts/GridMovement.cs
+++ Assets/Scripts/GridMovement.cs
@@ -6,6 +6,8 @@ public class GridMovement : MonoBehaviour
 {
     [SerializeField] private GridManager _gridManager;
     [SerializeField] private Vector2Int _gridPos;
+    public const float maxCountdown = 0.4f;
+    float movementCooldown = maxCountdown;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,5 +19,73 @@ public class GridMovement : MonoBehaviour
     {
         GameObject tile = _gridManager.GetTile(_gridPos.x, _gridPos.y);
         transform.position = tile.transform.position;
+        if (movementCooldown < maxCountdown)
+        {
+            movementCooldown += Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.RightArrow) && movementCooldown >= maxCountdown)
+        {
+            if ((_gridPos.x + 1) >= 0 && (_gridPos.x + 1) < _gridManager.numColumns)
+            {
+                _gridPos.x += 1;
+            }
+            else if ((_gridPos.x + 1) < 0)
+            {
+                _gridPos.x = _gridManager.numColumns - 1;
+            }
+            else if ((_gridPos.x + 1) >= _gridManager.numColumns)
+            {
+                _gridPos.x = 0;
+            }
+            movementCooldown = 0;
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow) && movementCooldown >= maxCountdown)
+        {
+            if ((_gridPos.x - 1) >= 0 && (_gridPos.x - 1) < _gridManager.numColumns)
+            {
+                _gridPos.x -= 1;
+            }
+            else if ((_gridPos.x - 1) < 0)
+            {
+                _gridPos.x = _gridManager.numColumns - 1;
+            }
+            else if ((_gridPos.x - 1) >= _gridManager.numColumns)
+            {
+                _gridPos.x = 0;
+            }
+            movementCooldown = 0;
+        }
+        else if (Input.GetKey(KeyCode.UpArrow) && movementCooldown >= maxCountdown)
+        {
+            if ((_gridPos.y + 1) >= 0 && (_gridPos.y + 1) < _gridManager.numColumns)
+            {
+                _gridPos.y += 1;
+            }
+            else if ((_gridPos.y + 1) < 0)
+            {
+                _gridPos.y = _gridManager.numColumns - 1;
+            }
+            else if ((_gridPos.y + 1) >= _gridManager.numColumns)
+            {
+                _gridPos.y = 0;
+            }
+            movementCooldown = 0;
+        }
+        else if (Input.GetKey(KeyCode.DownArrow) && movementCooldown >= maxCountdown)
+        {
+            if ((_gridPos.y - 1) >= 0 && (_gridPos.y - 1) < _gridManager.numColumns)
+            {
+                _gridPos.y -= 1;
+            }
+            else if ((_gridPos.y - 1) < 0)
+            {
+                _gridPos.y = _gridManager.numColumns - 1;
+            }
+            else if ((_gridPos.y - 1) >= _gridManager.numColumns)
+            {
+                _gridPos.y = 0;
+            }
+            movementCooldown = 0;
+        }
     }
 }
